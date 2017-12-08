ecb7e9be670769bbf937b2a9f7210041dc868ffd
Florry <viktor@frostdigital.se>
2017-12-08 15:51:09 +0100

    removes unused constants

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,4 @@
 module.exports = {
 
-    lang: {
-
-        subject: "Subject",
-        parsedSubject: "Subject details",
-        natsSubscribeOptions: "Nats subscribe options",
-        mustBeLoggedIn: "Must be logged in",
-        createQueueGroup: "Creates queue groups",
-        permissions: "Permissions",
-        forwardToHttp: "Forwards to http",
-        requestSchema: "Request body",
-        responseSchema: "Response body",
-        validateRequest: "Validates request data",
-        validateResponse: "Validates response data"
-
-    }
 
 };
