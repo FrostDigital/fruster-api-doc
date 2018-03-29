15170cfd803fec6b9104b9708c56092eaa3f0321
Florry <viktor@frostdigital.se>
2018-03-29 15:48:28 +0200

    applies bugfix

--- src/app/components/endpoint-container/EndpointContainer.js
+++ src/app/components/endpoint-container/EndpointContainer.js
@@ -6,7 +6,7 @@ export default class EndpointContainer extends React.Component {
 
     render() {
         return (
-            <div id={this.props.serviceName + "-" + (this.props.type || "service")} className={"service-container " + this.props.serviceName}>
+            <div id={this.props.serviceName + "-" + (this.props.type || "service")} className={"service-container " + this.props.serviceName + "-"}>
                 <a href={"#" + this.props.serviceName + "-" + (this.props.type || "service")}><h2>{this.props.serviceName}</h2></a>
 
                 {this.getEndpoints()}
