f4676fd7d4977bb24d53b61420f79dfa0f951925
Florry <viktor@frostdigital.se>
2018-07-05 10:36:34 +0200

    adds default react service worker

--- src/app/client.js
+++ src/app/client.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { hydrate } from "react-dom";
 import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+registerServiceWorker();
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOMContentLoaded, hydrating react app");
