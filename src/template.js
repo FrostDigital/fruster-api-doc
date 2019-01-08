3f43860a2e91726cf0abb7db855ff9ca76848a0d
Florry <viktor@frostdigital.se>
2019-01-08 14:17:12 +0100

    moves what is possible to assets/ rather than getting from other sources for faster / more consistent access

--- src/template.js
+++ src/template.js
@@ -6,7 +6,7 @@ export default ({ body, title, initialState }) => {
             <script>window._APP_STATE_ = ${initialState};</script>
             <title>${title}</title>
             <link rel="stylesheet" href="/assets/index.css" />
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.12.0/build/styles/googlecode.min.css">
+            <link rel="stylesheet" href="assets/googlecode.min.css">
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
         </head>
 
@@ -14,10 +14,10 @@ export default ({ body, title, initialState }) => {
             <div id="root" title="Documentation was generated on ${new Date().toJSON()}">${body}</div>
         </body>
 
-        <script src="https://code.jquery.com/jquery-3.1.0.js" integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/json-schema-faker/0.3.4/json-schema-faker.min.js"></script>
+        <script src="assets/jquery-3.1.0.js" integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="assets/json-schema-faker.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://github.com/TexKiller/node-docson/releases/download/v0.4.6/node-docson.min.js"></script> 
+        <script type="text/javascript" src="assets/node-docson.min.js"></script> 
 
         <script src="assets/client.js"></script>
         <script src="assets/babel-polyfill.js"/>
