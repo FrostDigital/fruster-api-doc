01a6c002736392e23dffe30ed240492a6f157ddd
Viktor Söderström <viktor@frostdigital.se>
2019-10-29 12:05:35 +0100

    [FIX / FEATURE] Bug fixes and api endpoints (for use by vscode extension) (#68)
    * quick fix for ip
    
    * fixes
    
    * fixes so that service client generator generates necesary necessary data if it's not already cached
    
    * adds some new endpoints

--- src/server.js
+++ src/server.js
@@ -29,274 +29,297 @@ let cachedHtml;
 
 (async function () {
 
-    await bus.connect({ address: config.bus });
+	await bus.connect({ address: config.bus });
 
-    require("fruster-health").start();
+	require("fruster-health").start();
 
-    await startServer();
+	await startServer();
 
 }());
 
 function startServer() {
-    app.use(compress());
+	app.use(compress());
 
-    app.use("/assets", express.static(path.resolve(`${__dirname}/assets`)));
+	app.use("/assets", express.static(path.resolve(`${__dirname}/assets`)));
 
-    app.post("/reset-cache", (req, res) => {
-        resetCache();
+	app.post("/reset-cache", (req, res) => {
+		resetCache();
 
-        res.status(200);
-        res.end();
-    });
+		res.status(200);
+		res.end();
+	});
 
-    app.get("/service-client/:serviceName", async (req, res) => {
-        try {
-            const type = "service";
-            const serviceName = req.params.serviceName;
-            const endpoints = endpointsByType.service[serviceName];
+	app.get("/service-list", async (req, res) => {
+		if (!endpointsByType.service.length)
+			await handleGenerateApiDoc(req, res, true);
 
-            if (!endpointsByType.service[serviceName]) {
-                res.end("<html><body><h1>No data found</h1><h2>run the api doc at least once and try again</h2></body></html>");
-                return;
-            }
+		res.json({ services: Object.keys(endpointsByType.service) });
+	});
 
-            const options = { serviceName, type, endpoints, subjects: req.query.subjects };
+	app.get("/service/:serviceName/endpoints", async (req, res) => {
+		if (!endpointsByType.service.length)
+			await handleGenerateApiDoc(req, res, true);
 
-            const serviceClientGenerator = new ServiceClientGenerator(options);
-            const className = ViewUtils.replaceAll(_.startCase(serviceName), " ", "") + "Client";
+		const serviceName = req.params.serviceName;
+		const endpoints = endpointsByType.service[serviceName].map(({ subject, docs: { description } }) => ({ subject, description }));
 
-            const file = serviceClientGenerator.toJavascriptClass();
+		res.json({ endpoints });
+	});
 
-            res.setHeader("Content-type", "application/javascript");
-            res.setHeader("Content-disposition", `attachment; filename=${className}.js`);
+	app.get("/service-client/:serviceName", async (req, res) => {
+		try {
+			const type = "service";
+			const serviceName = req.params.serviceName;
 
-            res.end(file);
-        } catch (err) {
-            log.warn(err);
-            res.end(`<html><body>Could not generate service client. <br/>Reason:  <br/><code><pre>${util.inspect(err, null, null)}</pre></code></body></html>`);
-        }
-    });
+			if (!endpointsByType.service[serviceName])
+				await handleGenerateApiDoc(req, res, true);
 
-    app.get("/", async (req, res) => {
-        try {
-            res.setHeader("Cache-Control", "max-age=300");
+			const endpoints = endpointsByType.service[serviceName];
 
-            if (req.query.resetCache)
-                resetCache();
+			const options = { serviceName, type, endpoints, subjects: req.query.subjects };
 
-            /** If there is cached html we send this right away but let the server process the rest, this will speed up avarage load times. */
-            if (cachedHtml)
-                res.send(cachedHtml);
+			const serviceClientGenerator = new ServiceClientGenerator(options);
+			const className = ViewUtils.replaceAll(_.startCase(serviceName), " ", "") + "Client";
 
-            let metadataResponses = [];
+			const file = serviceClientGenerator.toJavascriptClass();
 
-            if (req.query.project) { /** Lets you input an url to log viewer in order to use that to get metadata */
-                const url = `http://${req.query.project}-log-viewer.c4.fruster.se/api/bus/request`;
+			res.setHeader("Content-type", "application/javascript");
+			res.setHeader("Content-disposition", `attachment; filename=${className}.js`);
 
-                console.log("using", url);
+			res.end(file);
+		} catch (err) {
+			log.warn(err);
+			res.end(`<html><body>Could not generate service client!!. <br/>Reason:  <br/><code><pre>${util.inspect(err, null, null)}</pre></code></body></html>`);
+		}
+	});
 
-                metadataResponses = await utils.httpRequest("POST", url, {
-                    maxResponses: 10000,
-                    message: {},
-                    subject: "metadata"
-                });
-            } else
-                metadataResponses = await bus.requestMany({
-                    skipOptionsRequest: true,
-                    subject: "metadata",
-                    maxResponses: 10000,
-                    message: { reqId: uuid.v4() }
-                });
+	app.get("/", async (req, res) => handleGenerateApiDoc(req, res));
+
+	async function handleGenerateApiDoc(req, res, returnAfter) {
+		try {
+			if (!returnAfter)
+				res.setHeader("Cache-Control", "max-age=300");
+
+			if (req.query.resetCache)
+				resetCache();
+
+			/** If there is cached html we send this right away but let the server process the rest, this will speed up avarage load times. */
+			if (cachedHtml)
+				if (returnAfter)
+					return;
+				else
+					res.send(cachedHtml);
+
+			let metadataResponses = [];
+
+			if (req.query.project) { /** Lets you input an url to log viewer in order to use that to get metadata */
+				const url = `http://${req.query.project}-log-viewer.c4.fruster.se/api/bus/request`;
+
+				console.log("using", url);
+
+				metadataResponses = await utils.httpRequest("POST", url, {
+					maxResponses: 10000,
+					message: {},
+					subject: "metadata"
+				});
+			} else
+				metadataResponses = await bus.requestMany({
+					skipOptionsRequest: true,
+					subject: "metadata",
+					maxResponses: 10000,
+					message: { reqId: uuid.v4() }
+				});
+
+			let schemasWithErrors;
+			const promises = [];
+			const allEndpoints = new Set();
+
+			metadataResponses.forEach(response => {
+				if (!response.from) /** Just in case this happens we don't want the whole page to not load. */
+					response.from = { service: "", instanceId: "" };
+
+				const serviceName = response.from && response.from.service === "n/a" ? response.from.instanceId : response.from ? response.from.service : "na";
+				const fixedServiceName = serviceName.replace("n/a", "na");
+
+				if (!response.data)
+					return;
+
+				const promise = utils.derefJsonSchema(response.data.schemas, fixedServiceName)
+					.then((derefResp) => {
+						if (!derefResp)
+							return;
+
+						const schemas = derefResp.schemas;
+
+						if (derefResp.errors && Object.keys(derefResp.errors).length > 0) {
+							schemasWithErrors = {};
+							schemasWithErrors[fixedServiceName] = []
+
+							for (const error of derefResp.errors) {
+								console.log(error);
+								const jsonSchemaForError = response.data.schemas.find(s => s.id === error.id);
+								error.schema = jsonSchemaForError;
+
+								schemasWithErrors[fixedServiceName].push(error);
+							}
+						}
+
+						response.data.exposing.map((object, i) => {
+							allEndpoints.add(object.subject);
+
+							if (object.subject.includes("http")) parseEndpoint(object, 2, "http", schemas, fixedServiceName, response.from.instanceId);
+							else if (object.subject.includes("ws")) parseEndpoint(object, 2, "ws", schemas, fixedServiceName, response.from.instanceId);
+							else parseEndpoint(object, 0, "service", schemas, fixedServiceName, response.from.instanceId);
+						});
+					});
+
+				promises.push(promise);
+			});
+
+			await Promise.all(promises);
 
 
-            let schemasWithErrors;
-            const promises = [];
-            const allEndpoints = new Set();
+			Object.keys(endpointsByType).forEach(endpointType => sortAfterEndpointName(endpointsByType[endpointType]));
 
-            metadataResponses.forEach(response => {
-                if (!response.from) /** Just in case this happens we don't want the whole page to not load. */
-                    response.from = { service: "", instanceId: "" };
+			const state = { endpointsByType, schemasPerService, schemasWithErrors, allEndpoints: Array.from(allEndpoints), config, generatedDate: new Date().toJSON() };
+			const appString = renderToString(<App {...state} />);
+			const renderedHtml = template({
+				body: appString,
+				title: `${config.projectName} API documentation`,
+				initialState: JSON.stringify(state).split("\n").join("")
+			});
 
-                const serviceName = response.from && response.from.service === "n/a" ? response.from.instanceId : response.from ? response.from.service : "na";
-                const fixedServiceName = serviceName.replace("n/a", "na");
+			cachedHtml = renderedHtml;
 
-                if (!response.data)
-                    return;
+			if (!returnAfter)
+				if (!res.headersSent)
+					res.send(cachedHtml);
+		} catch (err) {
+			log.error(err);
 
-                const promise = utils.derefJsonSchema(response.data.schemas, fixedServiceName)
-                    .then((derefResp) => {
-                        if (!derefResp)
-                            return;
+			if (!returnAfter)
+				res.json(err);
+		}
+	}
 
-                        const schemas = derefResp.schemas;
-
-                        if (derefResp.errors && Object.keys(derefResp.errors).length > 0) {
-                            schemasWithErrors = {};
-                            schemasWithErrors[fixedServiceName] = []
-
-                            for (const error of derefResp.errors) {
-                                console.log(error);
-                                const jsonSchemaForError = response.data.schemas.find(s => s.id === error.id);
-                                error.schema = jsonSchemaForError;
-
-                                schemasWithErrors[fixedServiceName].push(error);
-                            }
-                        }
-
-                        response.data.exposing.map((object, i) => {
-                            allEndpoints.add(object.subject);
-
-                            if (object.subject.includes("http")) parseEndpoint(object, 2, "http", schemas, fixedServiceName, response.from.instanceId);
-                            else if (object.subject.includes("ws")) parseEndpoint(object, 2, "ws", schemas, fixedServiceName, response.from.instanceId);
-                            else parseEndpoint(object, 0, "service", schemas, fixedServiceName, response.from.instanceId);
-                        });
-                    });
-
-                promises.push(promise);
-            });
-
-            await Promise.all(promises);
-
-
-            Object.keys(endpointsByType).forEach(endpointType => sortAfterEndpointName(endpointsByType[endpointType]));
-
-            const state = { endpointsByType, schemasPerService, schemasWithErrors, allEndpoints: Array.from(allEndpoints), config, generatedDate: new Date().toJSON() };
-            const appString = renderToString(<App {...state} />);
-            const renderedHtml = template({
-                body: appString,
-                title: `${config.projectName} API documentation`,
-                initialState: JSON.stringify(state).split("\n").join("")
-            });
-
-            cachedHtml = renderedHtml;
-
-            if (!res.headersSent)
-                res.send(cachedHtml);
-
-        } catch (err) {
-            log.error(err);
-            res.json(err);
-        }
-    });
-
-    app.listen(port);
-    console.log(` 
+	app.listen(port);
+	console.log(`
 
 ================================================
     Fruster
 ================================================
    █████╗ ██████╗ ██╗    ██████╗  ██████╗  ██████╗
   ██╔══██╗██╔══██╗██║    ██╔══██╗██╔═══██╗██╔════╝
-  ███████║██████╔╝██║    ██║  ██║██║   ██║██║     
-  ██╔══██║██╔═══╝ ██║    ██║  ██║██║   ██║██║     
+  ███████║██████╔╝██║    ██║  ██║██║   ██║██║
+  ██╔══██║██╔═══╝ ██║    ██║  ██║██║   ██║██║
   ██║  ██║██║     ██║    ██████╔╝╚██████╔╝╚██████╗
   ╚═╝  ╚═╝╚═╝     ╚═╝    ╚═════╝  ╚═════╝  ╚═════╝
 ================================================
     Server running at http://localhost:${port}/
 ================================================
     `);
-    console.log(`Server running at http://localhost:${port}/`);
+	console.log(`Server running at http://localhost:${port}/`);
 }
 
 /**
  * Parses an endpoint into a format that can be used by the renderer.
- * 
+ *
  * @param {Object} object response object
  * @param {Number} splitIndex index of endpoint identifier (http.post.>>user<< for http and >>user-service<<.create-user for service).
- * @param {String} type type of endpoint 
+ * @param {String} type type of endpoint
  * @param {Array<Object>} schemas schemas for response
  * @param {String} serviceName name of service
- * @param {String} instanceId 
+ * @param {String} instanceId
  */
 function parseEndpoint(object, splitIndex, type, schemas, serviceName, instanceId) {
-    if (!object.subject) {
-        console.log("Could not parse endpoint; No subject...?", object);
-        return;
-    }
+	if (!object.subject) {
+		console.log("Could not parse endpoint; No subject...?", object);
+		return;
+	}
 
-    const splits = object.subject.split(".");
+	const splits = object.subject.split(".");
 
-    if (splits[splitIndex] === "health")
-        return;
+	if (splits[splitIndex] === "health")
+		return;
 
-    object.instanceId = instanceId;
-    object.serviceName = serviceName;
-    object.schemas = schemas.filter(s => s.id === object.requestSchema || s.id === object.responseSchema);
+	object.instanceId = instanceId;
+	object.serviceName = serviceName;
+	object.schemas = schemas.filter(s => s.id === object.requestSchema || s.id === object.responseSchema);
 
-    if (!endpointsByType[type][splits[splitIndex]])
-        endpointsByType[type][splits[splitIndex]] = [];
+	if (!endpointsByType[type][splits[splitIndex]])
+		endpointsByType[type][splits[splitIndex]] = [];
 
-    endpointsByType[type][splits[splitIndex]] = ViewUtils.addUnique(object, endpointsByType[type][splits[splitIndex]]);
+	endpointsByType[type][splits[splitIndex]] = ViewUtils.addUnique(object, endpointsByType[type][splits[splitIndex]]);
 
-    if (!object.cUrl && object.subject.includes("http"))
-        object.cUrl = getCUrlFromEndpoint(object, schemas);
+	if (!object.cUrl && object.subject.includes("http"))
+		object.cUrl = getCUrlFromEndpoint(object, schemas);
 
-    // if (!schemasPerService[serviceName])
-    //     schemasPerService[serviceName] = schemas;
+	// if (!schemasPerService[serviceName])
+	//     schemasPerService[serviceName] = schemas;
 }
 
 
 /**
  * Generates a cUrl from an endpoint.
- * 
+ *
  * @param {Object} endpoint endpoint data
- * @param {Array<Object>} schemas 
+ * @param {Array<Object>} schemas
  */
 function getCUrlFromEndpoint(endpoint, schemas) {
-    const requestSchema = schemas.find(s => s && s.id === endpoint.requestSchema);
-    const parsedSubject = ViewUtils.parseSubjectToAPIUrl(endpoint.subject);
+	const requestSchema = schemas.find(s => s && s.id === endpoint.requestSchema);
+	const parsedSubject = ViewUtils.parseSubjectToAPIUrl(endpoint.subject);
 
-    let body;
+	let body;
 
-    /** Adds request body if endpoint has a request schema */
-    if (requestSchema)
-        body = JSON.stringify(requestSchema.sample);
+	/** Adds request body if endpoint has a request schema */
+	if (requestSchema)
+		body = JSON.stringify(requestSchema.sample);
 
-    /** if endpoint uses wildcard method we use GET since wildcard/star is not a valid cUrl method */
-    if (parsedSubject.method === "*")
-        parsedSubject.method = "GET";
+	/** if endpoint uses wildcard method we use GET since wildcard/star is not a valid cUrl method */
+	if (parsedSubject.method === "*")
+		parsedSubject.method = "GET";
 
-    let authHeader = " ";
+	let authHeader = " ";
 
-    /** Adds a cookie field if authentication is needed to access endpoint */
-    if ((endpoint.permissions && endpoint.permissions.length > 0) || endpoint.mustBeLoggedIn)
-        authHeader = " --cookie jwt={{JWT_TOKEN}} ";
+	/** Adds a cookie field if authentication is needed to access endpoint */
+	if ((endpoint.permissions && endpoint.permissions.length > 0) || endpoint.mustBeLoggedIn)
+		authHeader = " --cookie jwt={{JWT_TOKEN}} ";
 
-    const cUrl = `curl -X ${parsedSubject.method} ${authHeader ? `${authHeader}` : ""} ${body ? `-H "Content-Type: application/json" -d '${body}'` : ""} ${config.apiRoot + parsedSubject.url}`;
+	const cUrl = `curl -X ${parsedSubject.method} ${authHeader ? `${authHeader}` : ""} ${body ? `-H "Content-Type: application/json" -d '${body}'` : ""} ${config.apiRoot + parsedSubject.url}`;
 
-    return ViewUtils.replaceAll(cUrl, "  ", " ");
+	return ViewUtils.replaceAll(cUrl, "  ", " ");
 }
 
 /**
- * @param {Object} endpoints 
+ * @param {Object} endpoints
  */
 function sortAfterEndpointName(endpoints) {
-    if (endpoints) {
-        Object.keys(endpoints)
-            .forEach(serviceName => {
-                endpoints[serviceName] = endpoints[serviceName].sort((a, b) => {
-                    const aU = a.subject.toUpperCase();
-                    const bU = b.subject.toUpperCase();
+	if (endpoints) {
+		Object.keys(endpoints)
+			.forEach(serviceName => {
+				endpoints[serviceName] = endpoints[serviceName].sort((a, b) => {
+					const aU = a.subject.toUpperCase();
+					const bU = b.subject.toUpperCase();
 
-                    if (aU > bU) return 1;
-                    else if (aU < bU) return -1;
-                    else return 0;
-                });
-            });
-    }
+					if (aU > bU) return 1;
+					else if (aU < bU) return -1;
+					else return 0;
+				});
+			});
+	}
 }
 
 function resetCache() {
-    console.log("Resetting cache");
+	console.log("Resetting cache");
 
-    schemasPerService = {};
-    endpointsByType = { http: {}, service: {}, ws: {} };
-    cachedHtml = undefined;
+	schemasPerService = {};
+	endpointsByType = { http: {}, service: {}, ws: {} };
+	cachedHtml = undefined;
 
-    console.log("Reset result:",
-        "\n endpointsByType.http:", Object.keys(endpointsByType.http).length,
-        "\n endpointsByType.service:", Object.keys(endpointsByType.service).length,
-        "\n endpointsByType.ws:", Object.keys(endpointsByType.ws).length,
-        "\n schemasPerService:", Object.keys(schemasPerService).length,
-        "\n cachedHtml === undefined:", cachedHtml === undefined);
+	console.log("Reset result:",
+		"\n endpointsByType.http:", Object.keys(endpointsByType.http).length,
+		"\n endpointsByType.service:", Object.keys(endpointsByType.service).length,
+		"\n endpointsByType.ws:", Object.keys(endpointsByType.ws).length,
+		"\n schemasPerService:", Object.keys(schemasPerService).length,
+		"\n cachedHtml === undefined:", cachedHtml === undefined);
 }
--- src/utils/ViewUtils.js
+++ src/utils/ViewUtils.js
@@ -3,7 +3,7 @@ const config = require("../../config");
 
 class ViewUtils {
 
-    constructor() { }
+	constructor() { }
 
     /**
      * @typedef {Object} ParsedSubject
@@ -13,148 +13,155 @@ class ViewUtils {
 
     /**
      * Parses a subject, picks out the method and transforms to url.
-     * 
+     *
      * @param {String} subject
-     * 
+     *
      * @return {ParsedSubject}
      */
-    static parseSubjectToAPIUrl(subject) {
-        let outputURL = subject;
-        outputURL = module.exports.replaceAll(outputURL, ".", "/");
-        outputURL = outputURL.replace("http/", "");
+	static parseSubjectToAPIUrl(subject) {
+		let outputURL = subject;
+		outputURL = module.exports.replaceAll(outputURL, ".", "/");
+		outputURL = outputURL.replace("http/", "");
 
-        const indexOfFirstSlash = outputURL.indexOf("/");
-        const method = outputURL.substring(0, indexOfFirstSlash);
+		const indexOfFirstSlash = outputURL.indexOf("/");
+		const method = outputURL.substring(0, indexOfFirstSlash);
 
-        outputURL = outputURL.substring(indexOfFirstSlash);
+		outputURL = outputURL.substring(indexOfFirstSlash);
 
-        return {
-            method: method.toUpperCase(),
-            url: outputURL
-        };
-    }
+		return {
+			method: method.toUpperCase(),
+			url: outputURL
+		};
+	}
 
     /**
  * Replaces all instances of word in string
- * 
+ *
  * @param {String} target target string
  * @param {String} search string to be replaced
  * @param {String} replacement string to replace with
- * 
+ *
  * @return {String} {target} string with {search} replaced by {replacement}
  */
-    static replaceAll(target, search, replacement) {
-        return target.split(search).join(replacement);
-    }
+	static replaceAll(target, search, replacement) {
+		if (!target.split) {
+			console.log("\n");
+			console.log(require("util").inspect(target, null, null, true));
+			console.log("\n");
+			return target;
+		}
+
+		return target.split(search).join(replacement);
+	}
 
     /**
      * Adds item to array only if it does not already exist.
-     * 
+     *
      * @param {Object} object object to add to array
      * @param {Array} array array to add object to
-     * 
+     *
      * @return {Array}
      */
-    static addUnique(object, array) {
-        const objectExists = array.find((endpoint, index, array) => {
-            /** Makes sure we do not add the same endpoint twice */
-            const isObject = endpoint.subject === object.subject && endpoint.serviceName.split(".")[0] === object.serviceName.split(".")[0];
+	static addUnique(object, array) {
+		const objectExists = array.find((endpoint, index, array) => {
+			/** Makes sure we do not add the same endpoint twice */
+			const isObject = endpoint.subject === object.subject && endpoint.serviceName.split(".")[0] === object.serviceName.split(".")[0];
 
-            if (isObject)
-                array[index] = object;
+			if (isObject)
+				array[index] = object;
 
-            return isObject;
-        });
+			return isObject;
+		});
 
-        if (!objectExists)
-            array.push(object);
+		if (!objectExists)
+			array.push(object);
 
-        return array;
-    }
+		return array;
+	}
 
     /**
      * Adds colors to selected words, defined in the config.
-     * 
-     * @param {String} string 
+     *
+     * @param {String} string
      */
-    static getColorCodedTitle(string) {
-        const colorCodedWords = Object.keys(config.colorCodedWords).reverse();
+	static getColorCodedTitle(string) {
+		const colorCodedWords = Object.keys(config.colorCodedWords).reverse();
 
-        for (let i = 0; i < colorCodedWords.length; i++) {
-            string = ViewUtils.replaceAll(string, colorCodedWords[i],
-                `<span class="${config.colorCodedWords[colorCodedWords[i]]}">${colorCodedWords[i]}</span>`);
-        }
+		for (let i = 0; i < colorCodedWords.length; i++) {
+			string = ViewUtils.replaceAll(string, colorCodedWords[i],
+				`<span class="${config.colorCodedWords[colorCodedWords[i]]}">${colorCodedWords[i]}</span>`);
+		}
 
-        return string;
-    }
+		return string;
+	}
 
     /**
      * Goes through keys in object (as well as its sub objects and arrays), sorts the keys and reinserts data in sorted order.
-     * 
+     *
      * @param {Object} obj object to sort keys for
      */
-    static sortObject(obj) {
-        if ((obj instanceof Array) || !(obj instanceof Object))
-            return obj;
+	static sortObject(obj) {
+		if ((obj instanceof Array) || !(obj instanceof Object))
+			return obj;
 
-        return sortLayer(obj);
+		return sortLayer(obj);
 
-        function sortLayer(layer) {
-            const output = {};
+		function sortLayer(layer) {
+			const output = {};
 
-            Object.keys(layer).sort().forEach(key => {
-                if (!!layer[key] && typeof layer[key] === "object" && !(layer[key] instanceof Array)) {
-                    /** If object, loop through another layer */
-                    output[key] = sortLayer(layer[key]);
-                } else if (!!layer[key] && (layer[key] instanceof Array)) {
-                    /**If array; sort array */
-                    layer[key].sort();
+			Object.keys(layer).sort().forEach(key => {
+				if (!!layer[key] && typeof layer[key] === "object" && !(layer[key] instanceof Array)) {
+					/** If object, loop through another layer */
+					output[key] = sortLayer(layer[key]);
+				} else if (!!layer[key] && (layer[key] instanceof Array)) {
+					/**If array; sort array */
+					layer[key].sort();
 
-                    if (typeof layer[key][0] === "object") {
-                        /** If array contains objects; loop through and sort each object */
-                        const sortedObj = sortLayer(layer[key]);
-                        output[key] = [];
+					if (typeof layer[key][0] === "object") {
+						/** If array contains objects; loop through and sort each object */
+						const sortedObj = sortLayer(layer[key]);
+						output[key] = [];
 
-                        Object.keys(sortedObj).forEach(sortedObjkey => {
-                            /** Add back objects in an array (To prevent result being converted from array to object while using Object.keys) */
-                            if (!output[key])
-                                output[key] = [];
+						Object.keys(sortedObj).forEach(sortedObjkey => {
+							/** Add back objects in an array (To prevent result being converted from array to object while using Object.keys) */
+							if (!output[key])
+								output[key] = [];
 
-                            output[key].push(sortedObj[sortedObjkey]);
-                        });
-                    } else {
-                        output[key] = layer[key];
-                    }
-                } else {
-                    output[key] = layer[key];
-                }
-            });
+							output[key].push(sortedObj[sortedObjkey]);
+						});
+					} else {
+						output[key] = layer[key];
+					}
+				} else {
+					output[key] = layer[key];
+				}
+			});
 
-            return output;
-        }
-    }
+			return output;
+		}
+	}
 
     /**
      * Loops through an object or array.
      *
      * @param {Object | Array} toLoop
      * @param {Function} handler
-     * 
+     *
      * @return {Array}
     */
-    static sortedForEach(toLoop, handler) {
-        if (!toLoop || Object.keys(toLoop).length === 0)
-            return [];
+	static sortedForEach(toLoop, handler) {
+		if (!toLoop || Object.keys(toLoop).length === 0)
+			return [];
 
-        let i = 0;
+		let i = 0;
 
-        return Object.keys(toLoop)
-            .sort()
-            .map(key => {
-                i++;
-                return handler(toLoop[key], key, i);
-            });
-    }
+		return Object.keys(toLoop)
+			.sort()
+			.map(key => {
+				i++;
+				return handler(toLoop[key], key, i);
+			});
+	}
 
 }
 
